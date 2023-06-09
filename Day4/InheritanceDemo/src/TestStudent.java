11c5c8c132c80aabcb76949525a0c1e49208af73
Ruturaj Hodage <noogleindia@gmail.com>
2023-06-09 23:30:59 +0530

    Day4

---
+++ Day4/InheritanceDemo/src/TestStudent.java
@@ -0,0 +1,11 @@
+
+public class TestStudent {
+
+	public static void main(String[] args) {
+		Student s=new Student();
+		Student s1=new Student("S","Thor","7377",99,98,97);
+		System.out.println(s1);
+
+	}
+
+}
